d081e6b30792d035fbc2d4347cefa33b4da0dcde
Matthew Young <matthew.young@developersinstitute.co.nz>
2023-11-23 17:20:20 +1300

    finsihed creating project structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./index.css";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import TopPicks from "./routes/TopPicks";
+import Contact from "./routes/Contact";
+import Music from "./routes/Music";
+
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/top-picks" element={<TopPicks />} />
+        <Route path="/music" element={<Music />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
