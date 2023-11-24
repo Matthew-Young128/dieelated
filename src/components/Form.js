614c58b8d794f37a6537516a015724d29cc1d28b
Matthew Young <matthew.young@developersinstitute.co.nz>
2023-11-25 02:14:26 +1300

    implemented email.js into form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,45 @@
 import "./FormStyles.css";
-
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Form = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_lsn3yfn",
+        "template_olhe7u8",
+        form.current,
+        "yvgxg-ndkzS0R3YN2"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div className="form">
-      <form>
+      <form ref={form} onSubmit={sendEmail}>
         <label>Your Name</label>
-        <input type="text"></input>
+        <input type="text" name="user_name"></input>
         <label>Your Email</label>
-        <input type="Email"></input>
+        <input type="Email" name="user_email"></input>
         <label>Subject</label>
         <input type="text"></input>
         <label>Message</label>
-        <textarea rows="6" placeholder="Type your message here" />
+        <textarea
+          rows="6"
+          placeholder="Type your message here"
+          name="message"
+        />
         <button className="contact-btn">Submit</button>
       </form>
     </div>
