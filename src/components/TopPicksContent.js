7c490b38103c880577b992fadf40598f08e5a1ea
Matthew Young <matthew.young@developersinstitute.co.nz>
2023-11-29 17:26:07 +1300

    fixed moderate error

--- src/components/TopPicksContent.js
+++ src/components/TopPicksContent.js
@@ -8,72 +8,80 @@ const TopPicksContent = () => {
     <div>
       <div className="top-picks-container">
         <ul className="top-picks">
-          <li className="pick">
-            <p className="orange">DATSKO -</p>
-            <p>REMEMBRANCE</p>
-          </li>
-          <li className="pick">
-            <p className="orange">DJ HEARSTRING -</p>
-            <p>Will You Remember Me When I'm Gone</p>
-          </li>
-          <li className="pick">
-            <p className="orange">southstar -</p>
-            <p>Hold</p>
-          </li>
-          <li className="pick">
-            <p className="orange">scruz -</p>
-            <p>Keep</p>
-          </li>
-          <li className="pick">
-            <p className="orange">Kenya Grace -</p>
-            <p>Only In My Mind</p>
-          </li>
-          <li className="pick">
-            <p className="orange">Disclosure -</p>
-            <p>Higher Than Ever Before</p>
-            <p className="orange">ATRIP Remix</p>
-          </li>
-          <li className="pick">
-            <p className="orange">KAM-BU -</p>
-            <p>SPACE</p>
-          </li>
-          <li className="pick">
-            <p className="orange">Jayda G -</p>
-            <p>Both Of Us</p>
-          </li>
-          <li className="pick">
-            <p className="orange">MPH -</p>
-            <p>Bad Day</p>
-          </li>
-          <li className="pick">
-            <p className="orange">Harry Hayes -</p>
-            <p>Vesper</p>
-          </li>
-          <li className="pick">
-            <p className="orange">Macabre Plaza -</p>
-            <p>Dimly Lit Small Town</p>
-          </li>
-          <li className="pick">
-            <p className="orange">Sammy Virji, Flowdan -</p>
-            <p>Shella Verse</p>
-          </li>
-          <li className="pick">
-            <p className="orange">N4TEE -</p>
-            <p>94 BITZZ</p>
-          </li>
-          <li className="pick">
-            <p className="orange">Y U QT -</p>
-            <p>Y'all Ready For Dis</p>
-          </li>
-          <li className="pick">
-            <p className="orange">Simula -</p>
-            <p>Bonesaw</p>
-          </li>
-          <li className="pick">
-            <p className="orange">Jimi Jules -</p>
-            <p>My City's On Fire</p>
-            <p className="orange">Anyma & Cassian Remix</p>
-          </li>
+          {/* I added this anchor tag to allow the user to stream the tracks in one place*/}
+          <a
+            href="https://soundcloud.com/matthew-young-493848941/sets/die-elated-top-picks"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <li className="pick">
+              <p className="orange">DATSKO -</p>
+              <p>REMEMBRANCE</p>
+            </li>
+            <li className="pick">
+              <p className="orange">DJ HEARSTRING -</p>
+              <p>Will You Remember Me When I'm Gone</p>
+            </li>
+            <li className="pick">
+              <p className="orange">southstar -</p>
+              <p>Hold</p>
+            </li>
+            <li className="pick">
+              <p className="orange">scruz -</p>
+              <p>Keep</p>
+            </li>
+            <li className="pick">
+              <p className="orange">Kenya Grace -</p>
+              <p>Only In My Mind</p>
+            </li>
+            <li className="pick">
+              <p className="orange">Disclosure -</p>
+              <p>Higher Than Ever Before</p>
+              <p className="orange">ATRIP Remix</p>
+            </li>
+            <li className="pick">
+              <p className="orange">KAM-BU -</p>
+              <p>SPACE</p>
+            </li>
+            <li className="pick">
+              <p className="orange">Jayda G -</p>
+              <p>Both Of Us</p>
+            </li>
+            <li className="pick">
+              <p className="orange">MPH -</p>
+              <p>Bad Day</p>
+            </li>
+            <li className="pick">
+              <p className="orange">Harry Hayes -</p>
+              <p>Vesper</p>
+            </li>
+            <li className="pick">
+              <p className="orange">Macabre Plaza -</p>
+              <p>Dimly Lit Small Town</p>
+            </li>
+            <li className="pick">
+              <p className="orange">Sammy Virji, Flowdan -</p>
+              <p>Shella Verse</p>
+            </li>
+            <li className="pick">
+              <p className="orange">N4TEE -</p>
+              <p>94 BITZZ</p>
+            </li>
+            <li className="pick">
+              <p className="orange">Y U QT -</p>
+              <p>Y'all Ready For Dis</p>
+            </li>
+            <li className="pick">
+              <p className="orange">Simula -</p>
+              <p>Bonesaw</p>
+            </li>
+            <li className="pick">
+              <p className="orange">Jimi Jules -</p>
+              <p>My City's On Fire</p>
+              <p className="orange">Anyma & Cassian Remix</p>
+            </li>
+          </a>
         </ul>
         <img src={GoldStamp} alt="gold stamp" className="gold-stamp"></img>
       </div>
